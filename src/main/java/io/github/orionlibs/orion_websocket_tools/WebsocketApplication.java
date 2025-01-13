1885d0ff798f2a0ffa82466fb775c4d381c7c1b5
Dimitrios Efthymiou <efthymiou.dimitrios1@gmail.com>
2025-01-13 21:58:47 +0000

    1

--- src/main/java/io/github/orionlibs/orion_websocket_tools/WebsocketApplication.java
+++ src/main/java/io/github/orionlibs/orion_websocket_tools/WebsocketApplication.java
@@ -2,23 +2,25 @@ package io.github.orionlibs.orion_websocket_tools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @SpringBootApplication
 @EnableScheduling
 @EnableWebSecurity
+@EnableWebMvc
+@ComponentScan(basePackages =
+                {"io.github.orionlibs.orion_websocket_tools"})
 public class WebsocketApplication
 {
     public static void main(String[] args)
     {
         SpringApplication.run(WebsocketApplication.class, args);
     }
-
-
-
 
 
     /*@Bean
